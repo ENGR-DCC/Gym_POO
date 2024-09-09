7eb5e09381f1894061fb9b2319063cf13ccdfd55
ENGR-DCC <161765012+ENGR-DCC@users.noreply.github.com>
2024-09-08 22:05:08 -0500

    Update Entrenador.java

--- gimnasio/Entrenador.java
+++ gimnasio/Entrenador.java
@@ -11,16 +11,13 @@ public class Entrenador extends Persona {
         this.especialidad = especialidad;
     }
 
-    // Métodos
     public void asignarCliente(Cliente cliente) {
         clientesAsignados.add(cliente);
     }
 
     public void planificarEntrenamiento(Cliente cliente, Rutina rutina) {
-        // Lógica para planificar entrenamiento
     }
 
     public void consultarHorario() {
-        // Lógica para consultar horario
     }
 }
